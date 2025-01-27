0ff91524a1bca6141b7869e5f20a94f3f4a53ed5
Ashwin S <achu10@live.in>
2025-01-27 13:50:48 +0530

    restructure

--- 1-atlas/app-services/functions/retrieveApiGatewayApiKey.js
+++
@@ -1,8 +0,0 @@
-exports = async function(arg){
-  try {
-    return  context.values.get("ApiGatewayApiKeyRef");
-  } catch(err) {
-    console.log("Error occurred while executing function:", err.message);
-    return { error: err.message };
-  }
-}
